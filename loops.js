da78ef0096ddc9e1e2850baaad6c6c911ac52188
William Damm <wdamm@powerhrg.com>
2020-10-29 03:49:23 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,8 +3,12 @@
 
 
 function forLoop() {
-  if (i === 1) {
-    console.log('I am 1 strange loop.');
-  }
-  
+ for (let i = 0; i < 25; i++) {
+   if (i === 1) {
+     console.log ('I am 1 strange loop.')
+   }
+   else {
+     console.log('I am ' + i + 'vstrange loops.')
+   }
+ }
 }
