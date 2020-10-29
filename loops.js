436d0c42684a9af87d78e930137811101bc15815
William Damm <wdamm@powerhrg.com>
2020-10-29 04:11:14 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,10 +3,10 @@ array = []
 function forLoop(array) {
  for (let i = 0; i < 25; i++) {
    if (i === 1) {
-     array[i] = "I am ${i}"
+     array[i] = "I am ${i} strange loop."
    }
    else {
-     console.log('I am ' + i + ' strange loops.')
+     array[i] = "I am ${i} strange loops."
    }
  }
  return array;
