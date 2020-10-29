af40e83c83b304c003dfdb38649e107c327a43b5
William Damm <wdamm@powerhrg.com>
2020-10-29 04:10:32 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ array = []
 function forLoop(array) {
  for (let i = 0; i < 25; i++) {
    if (i === 1) {
-     console.log ('I am 1 strange loop.')
+     array[i] = "I am ${i}"
    }
    else {
      console.log('I am ' + i + ' strange loops.')
